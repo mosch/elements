499a8904bcc8e53522c47e7bcc858a28a52bbd03
Moritz Schwörer <mr.mosch@gmail.com>
2017-10-19 21:37:06 +0200

    move from glamor to css

--- documentation/src/components/Navigation.js
+++ documentation/src/components/Navigation.js
@@ -1,20 +1,5 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import { css } from 'glamor'
-
-const styles = {
-  list: css({
-    listStyle: 'none'
-  }),
-  item: css({
-    marginBottom: 'calc(1.56rem / 6)'
-  }),
-
-  navigation: css({
-    margin: '0',
-    flex: '0 0 14rem'
-  }),
-}
 
 export default ({ items }) => {
   const index = items.reduce((acc, val) => {
@@ -32,13 +17,13 @@ export default ({ items }) => {
   }, {})
 
   return (
-    <div {...styles.navigation}>
+    <div className="navigation">
       {Object.keys(index).map(type => {
         return (
-          <ul {...styles.list}>
+          <ul className="navigation-list">
             <h3 style={{ textTransform: 'capitalize' }}>{type}</h3>
             {Object.values(index[type]).map(({ name, path }) => (
-              <li {...styles.item}>
+              <li>
                 <Link
                   to={path}
                   style={{
