5e33fb0ef808e4fab6ba3665bae722ee4d535e91
Moritz Schwörer <mr.mosch@gmail.com>
2017-10-23 09:06:17 +0200

    use new color warn from theme

--- src/molecules/CountIndicator.jsx
+++ src/molecules/CountIndicator.jsx
@@ -3,34 +3,40 @@ import PropTypes from 'prop-types'
 import Circle from '../atoms/Circle'
 import Text from '../atoms/Text'
 import Absolute from '../atoms/Absolute'
+import Theme from '../behaviour/Theme'
 
-class CountIndicator extends React.Component {
-  static propTypes = {
-    /** Number to display */
-    count: PropTypes.number.isRequired,
-    /** Top offset */
-    top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /** Bottom offset */
-    bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /** Left offset */
-    left: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    /** Right offset */
-    right: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  }
+const CountIndicator = ({ count, ...props }) =>
+  count === 0 ? null : (
+    <Theme>
+      {({colorize}) => (
+        <Absolute {...props}>
+          <Circle color={colorize(props.color)} radius={18}>
+            <Text size="xs" color="white">
+              {count > 9 ? '9+' : count}
+            </Text>
+          </Circle>
+        </Absolute>
+      )}
+    </Theme>
+  )
 
-  render() {
-    const { count, ...props } = this.props
+CountIndicator.propTypes = {
+  /** Number to display */
+  count: PropTypes.number.isRequired,
+  /** Top offset */
+  top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Bottom offset */
+  bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Left offset */
+  left: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Right offset */
+  right: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Color */
+  color: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+}
 
-    return count === 0 ? null : (
-      <Absolute {...props}>
-        <Circle color="red" radius={18}>
-          <Text size="xs" color="white">
-            {count > 9 ? '9+' : count}
-          </Text>
-        </Circle>
-      </Absolute>
-    )
-  }
+CountIndicator.defaultProps = {
+  color: 'warn',
 }
 
 export default CountIndicator
